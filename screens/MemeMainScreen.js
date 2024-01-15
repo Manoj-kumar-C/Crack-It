5aba328d7ccfe7be84767fedd72b74ef69008d04
manoj-chandran <developermanoj2003@gmail.com>
2024-01-15 21:39:46 +0530

    main screen

---
+++ screens/MemeMainScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MemeMainScreen = () => {
+  return (
+    <View>
+      <Text>MemeMainScreen</Text>
+    </View>
+  )
+}
+
+export default MemeMainScreen
